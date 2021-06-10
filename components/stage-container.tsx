952d5dca96ecd1a46c28855d33d0ca98d4387d74
Digital Aksjon <post@digitalaksjon.no>
2021-06-10 16:26:33 +0200

    removed vimeo player

--- components/stage-container.tsx
+++ components/stage-container.tsx
@@ -43,7 +43,6 @@ export default function StageContainer({ stage, allStages }: Props) {
       <div className={styles.streamContainer}>
         {loginStatus === 'loggedIn' ? (
           <div className={cn(styles.stream, styleUtils.appear, styleUtils['appear-first'])}>
-          <iframe src="https://player.vimeo.com/video/560934825" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
           </div>
         ) : loginStatus === 'loading' ? null : (
           <ConfEntry onRegister={() => mutate()} />
